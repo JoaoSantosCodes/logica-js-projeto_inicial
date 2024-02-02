ec8c1417f67ebe8b3d4a08662b0e296505d0a41b
joao Santos <joaocarlosrh23@gmail.com>
2024-02-02 00:46:32 -0300

    edit

---
+++ app.js
@@ -0,0 +1,21 @@
+// Exibe um alerta com a mensagem de boas-vindas ao jogo do número secreto.
+alert('Boas-vindas ao jogo do número secreto');
+
+// Declara uma variável chamada numeroSecreto e atribui a ela o valor 5.
+let numeroSecreto = 2;
+
+// Exibe o número secreto no console para fins de depuração.
+console.log(numeroSecreto);
+
+// Usa o prompt para solicitar ao usuário que escolha um número entre 1 e 10 e armazena o valor na variável chute.
+let chute = prompt('Escolha um número entre 1 e 10');
+
+// Condição: verifica se o valor armazenado em chute é igual ao valor armazenado em numeroSecreto.
+if (numeroSecreto == chute) {
+    // Se a condição for verdadeira, exibe um alerta informando que o usuário descobriu o número secreto.
+
+    alert(`sso aí! Você descobriu o número secreto ${numeroSecreto}`);
+} else {
+    // Se a condição for falsa, exibe um alerta informando que houve um erro.
+    alert('ERROOOO!');
+}
